febf28c29a2dda95fb343dece210eac289b6545c
O~P~L <stevebickookumu@gmail.com>
2026-07-03 03:16:12 +0300

    Remove obsolete vercel configuration

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import { ACCESS_TOKEN } from './constants';
-import { navigate } from 'react-router-dom';
 
 const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
 
